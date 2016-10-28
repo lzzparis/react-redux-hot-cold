01bb1a89e989c97046b5303916351f6ff499eb9d
Lizzie Paris <lzz.paris@gmail.com>
2016-10-27 20:49:55 -0700

    Clear input upon guess

--- guess-form.js
+++ guess-form.js
@@ -4,15 +4,28 @@ var connect = require("react-redux").connect;
 var actions = require("./actions");
 
 var GuessForm = React.createClass({
+  getInitialState: function(){
+    return{
+      inputValue: ""
+    }
+  },
+  updateInput:function(event){
+    this.setState({inputValue:event.target.value})
+  },
+  resetInput:function(){
+    this.setState({inputValue:""});
+  },
   submitGuess: function(event){
     event.preventDefault();
     this.props.dispatch(actions.guessNumber(this.refs.guess.value));
     this.props.dispatch(actions.calcFeedback());
+    this.resetInput();
   },
   render: function(){
     return (
       <form>
-				<input type="text" ref="guess" id="userGuess" className="text" placeholder="Enter your Guess" required/>
+				<input type="text" ref="guess" id="userGuess" className="text" placeholder="Enter your Guess" 
+          onChange={this.updateInput} value={this.state.inputValue} required/>
       	<input type="submit" id="guessButton" className="button" name="submit" value="Guess" onClick={this.submitGuess}/>
       </form>
     );
