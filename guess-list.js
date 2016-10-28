69a54a1a308509a1951ebc3cbded087b3e92e814
Lizzie Paris <lzz.paris@gmail.com>
2016-10-27 21:34:47 -0700

    Force GuessList to re-render by adding Feedback in hidden p - should fix this

--- guess-list.js
+++ guess-list.js
@@ -3,17 +3,22 @@ var connect = require("react-redux").connect;
 
 
 var GuessList = function(props){
+
   var list = [];
   for(var i=0 ; i < props.guesses.length; i++){
-    list.push(<li>{props.guesses[i]}</li>);
+    list.push(<li key={i}>{props.guesses[i]}</li>);
   }
   return(
+    <div>
+    <p style={{display:"none"}}>{props.feedback}</p>
     <ul id="guessList" className="guessBox clearfix">{list}</ul>
+    </div>
   );
 };
 
 var mapStateToProps = function(state, props){
   return{
+    feedback:state.feedback,
     guesses: state.guesses
   }
 }
